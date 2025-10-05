b65dfdcd5f8171e8ecd661b1f6daa41dc23fa778
fish <th3fishmk@proton.me>
2025-10-05 01:46:45 -0600

    formatted

--- tauri-app/src/App.tsx
+++ tauri-app/src/App.tsx
@@ -17,21 +17,27 @@ function App() {
             <h1>Welcome to Tauri + React</h1>
 
             <div className="row">
-                <a href="https://vite.dev" target="_blank">
+                <a
+                    href="https://vite.dev"
+                    target="_blank">
                     <img
                         src="/vite.svg"
                         className="logo vite"
                         alt="Vite logo"
                     />
                 </a>
-                <a href="https://tauri.app" target="_blank">
+                <a
+                    href="https://tauri.app"
+                    target="_blank">
                     <img
                         src="/tauri.svg"
                         className="logo tauri"
                         alt="Tauri logo"
                     />
                 </a>
-                <a href="https://react.dev" target="_blank">
+                <a
+                    href="https://react.dev"
+                    target="_blank">
                     <img
                         src={reactLogo}
                         className="logo react"
@@ -46,8 +52,7 @@ function App() {
                 onSubmit={(e) => {
                     e.preventDefault();
                     greet();
-                }}
-            >
+                }}>
                 <input
                     id="greet-input"
                     onChange={(e) => setName(e.currentTarget.value)}
